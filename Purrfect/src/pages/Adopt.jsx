d4f2c1adb9353670d585e71e4ed78059f497154f
siddi <siddiqueearhaan@gmail.com>
2024-10-30 18:06:39 +0530

    banner

--- Purrfect/src/pages/Adopt.jsx
+++ Purrfect/src/pages/Adopt.jsx
@@ -1,13 +1,9 @@
 import React from "react";
-
+import logo from "../logo/adoptbanner.svg"
 const Adopt = () => {
   return (
-    <div className="p-10 bg-gray-100">
-      <h2 className="text-3xl font-bold text-purple-700">Adopt a Cat</h2>
-      <p className="mt-4 text-lg">
-        Find your perfect companion. Browse our available cats!
-      </p>
-      {/* Add cards for individual cats here */}
+    <div>
+      <img src={logo} alt="" />
     </div>
   );
 };
